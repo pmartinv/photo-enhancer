45f944138a7501aba52222a81d2bed62c596fc89
Pedro Martin <pedro.martin@vodafone.com>
2021-05-24 22:55:21 +0200

    First revision

---
+++ src/main/java/com/softmax/photoenhancer/PhotoenhancerApplication.java
@@ -0,0 +1,13 @@
+package com.softmax.photoenhancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoenhancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoenhancerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softmax/photoenhancer/PhotoenhancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softmax.photoenhancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoenhancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
